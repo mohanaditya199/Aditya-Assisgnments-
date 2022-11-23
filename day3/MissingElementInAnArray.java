ecc303fae339e17b28cddd9b88fe909e82b5f1cb
mohanaditya199 <118680249+mohanaditya199@users.noreply.github.com>
2022-11-23 15:18:52 -0500

    Add files via upload

---
+++ day3/MissingElementInAnArray.java
@@ -0,0 +1,45 @@
+package week1.day3;
+
+
+import java.util.Arrays;
+
+
+
+public class MissingElementInAnArray {
+
+	public static void main(String[] args) {
+	
+		int[] arr = {1,2,3,4,7,6,8};
+		
+		
+		//int length1 = arr.length;
+				//System.out.println(length1);
+		
+		
+		
+		Arrays.sort(arr);
+		
+		for (int i = arr[0]; i < arr.length; i++) {
+			//System.out.println(arr[i]);
+			
+			if (arr[i]!=i+1) {
+				System.out.println(i+1);
+				break;
+			}
+			
+			
+			
+			
+		}
+		
+	
+		
+		
+		
+		
+		
+		
+
+	}
+
+}
