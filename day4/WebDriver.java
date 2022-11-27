1dbe2d92f89514a98b4eef411930b6f33c3a39b0
mohanaditya199 <118680249+mohanaditya199@users.noreply.github.com>
2022-11-27 15:28:32 -0500

    Add files via upload

---
+++ day4/WebDriver.java
@@ -0,0 +1,17 @@
+package week1.day4;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class WebDriver {
+
+	public static void main(String[] args) {
+		
+		WebDriverManager.chromedriver().setup();
+		ChromeDriver driver = new ChromeDriver();
+		driver.get("http://leaftaps.com/opentaps/control/main");
+
+	}
+
+}
